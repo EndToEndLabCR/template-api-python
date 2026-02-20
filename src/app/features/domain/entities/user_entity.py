dce6291e6415c7b576a64ffff0dd12c31da11788
Génesis Morales <genesismoralesc1@gmail.com>
2026-02-20 14:39:34 -0600

    Update user_entity.py

--- src/app/features/domain/entities/user_entity.py
+++ src/app/features/domain/entities/user_entity.py
@@ -8,11 +8,12 @@ from src.shared.domain.value_objects.entity_id import EntityId
 
 class UserEntity(BaseEntity):
 
-    def __init__(self, id: EntityId, email: Email, first_name: str, last_name: str,
+    def __init__(self, id: EntityId, email: Email, first_name: str, last_name: str, password_hash: str,
                  created_at: Optional[datetime] = None,
                  updated_at: Optional[datetime] = None):
 
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
+        self.password_hash = password_hash
         super().__init__(id, created_at, updated_at)
