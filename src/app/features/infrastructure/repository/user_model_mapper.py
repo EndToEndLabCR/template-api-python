61536ec9b611eaca9c4100b3aa9c3ca6bf32084e
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-05-13 22:14:08 -0600

    feat: updating user model mapper and repository impl

--- src/app/features/infrastructure/repository/user_model_mapper.py
+++ src/app/features/infrastructure/repository/user_model_mapper.py
@@ -11,6 +11,8 @@ def map_model_to_entity(user_model: UserModel):
         first_name=user_model.first_name,
         last_name=user_model.last_name,
         password_hash=user_model.password_hash,
+        password_reset_token_hash=user_model.password_reset_token_hash,
+        password_reset_expires_at=user_model.password_reset_expires_at,
         created_at=user_model.created_at,
         updated_at=user_model.updated_at,
     )
